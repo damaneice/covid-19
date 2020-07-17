9346887e30b9d14f14117b4b29feb03e578f52cf
Damon Maneice <damaneice@gmail.com>
2020-07-16 21:27:05 -0400

    Sort Counties by total cases

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,46 +6,64 @@ import LineChart from "../components/linechart"
 import SEO from "../components/seo"
 import "./home.css"
 
-const createChartData = (county, counties) => {
+const createChartData = county => {
   const chartData = []
   let subset = []
-  for(let i = 0; i < counties[county].chart.length; i++){
-    subset.push(counties[county].chart[i])
+  for (let i = 0; i < county.chart.length; i++) {
+    subset.push(county.chart[i])
     if ((i + 1) % 7 === 0) {
-      let total = subset.reduce((acc, current) => acc + current.cases, 0);
-      chartData.push(total / subset.length);
+      let total = subset.reduce((acc, current) => acc + current.cases, 0)
+      chartData.push(total / subset.length)
       subset = []
     }
   }
   return chartData.map((y, index) => {
-    return {"x": index, "y": y}
+    return { x: index, y: y }
   })
 }
 
-const transformerCountyData = (edges) => {
+const transformerCountyData = edges => {
   const counties = {}
   edges.forEach(edge => {
     if (counties[edge.node.COUNTY]) {
-      counties[edge.node.COUNTY].total = counties[edge.node.COUNTY].total + parseInt(edge.node.Cases)
+      counties[edge.node.COUNTY].total =
+        counties[edge.node.COUNTY].total + parseInt(edge.node.Cases)
       counties[edge.node.COUNTY].newCases = edge.node.Cases
-      counties[edge.node.COUNTY].chart.push({"cases": edge.node.Cases, "date": edge.node.Date })
-    }
-    else {
-      counties[edge.node.COUNTY] = {newCases: 0, total: 0, chart: []}
+      counties[edge.node.COUNTY].chart.push({
+        cases: edge.node.Cases,
+        date: edge.node.Date,
+      })
+    } else {
+      counties[edge.node.COUNTY] = { newCases: 0, total: 0, chart: [] }
     }
   })
-  return counties
+  const countiesArray = Object.keys(counties).map(county => {
+    return {
+      name: county,
+      newCases: counties[county].newCases,
+      total: counties[county].total,
+      chart: counties[county].chart,
+    }
+  })
+  return countiesArray.sort((a, b) => b.total - a.total)
 }
-const CountyRow = ({ county, counties}) => {
+const CountyRow = ({ county }) => {
   return (
     <>
-      <div className="table-data table-cell"><p>{county}</p></div>
-      <div className="table-data table-cell"><p>{counties[county].total}</p></div>
-      <div className="table-data table-cell"><p>{counties[county].newCases}</p></div>
-      <div className="table-cell table-chart"><LineChart color={"#2196F3"} data={createChartData(county, counties)}/></div>
+      <div className="table-data table-cell">
+        <p>{county.name}</p>
+      </div>
+      <div className="table-data table-cell">
+        <p>{county.total}</p>
+      </div>
+      <div className="table-data table-cell">
+        <p>{county.newCases}</p>
+      </div>
+      <div className="table-cell table-chart">
+        <LineChart color={"#2196F3"} data={createChartData(county)} />
+      </div>
     </>
   )
-
 }
 
 const IndexPage = ({ data }) => {
@@ -57,25 +75,32 @@ const IndexPage = ({ data }) => {
       <SEO title="Home" />
       <div className="container">
         <div className="table">
-          <div key="county-header" className="table-header">COUNTY</div>
-          <div key="total-header" className="table-header">TOTAL</div>
-          <div key="new-cases-header" className="table-header">NEW</div>
-          <div key="chart-header" className="table-header">7 DAY ROLLING AVERAGE</div>
-          {
-            Object.keys(counties).map(county => {
-              return <CountyRow key={`${county}-row`} county={county} counties={counties}/>
-            })
-          }
+          <div key="county-header" className="table-header">
+            COUNTY
+          </div>
+          <div key="total-header" className="table-header">
+            TOTAL
+          </div>
+          <div key="new-cases-header" className="table-header">
+            NEW
+          </div>
+          <div key="chart-header" className="table-header">
+            7 DAY ROLLING AVERAGE
+          </div>
+          {counties.map(county => {
+            return <CountyRow key={`${county}-row`} county={county} />
+          })}
         </div>
       </div>
     </Layout>
   )
 }
 
-
 export const query = graphql`
   query HomepageQuery {
-    allCasesByCountyAndDateXlsxData(filter: {CASE_STATUS: {ne: "Probable"}}) {
+    allCasesByCountyAndDateXlsxData(
+      filter: { CASE_STATUS: { ne: "Probable" } }
+    ) {
       edges {
         node {
           COUNTY
