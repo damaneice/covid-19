8beb91226102f3662769aec97af59f32a7ebcf57
Damon Maneice <damaneice@gmail.com>
2020-07-26 18:47:24 -0400

    make graph be interactive

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,7 +1,7 @@
 import * as d3 from "d3"
 import React, { useEffect, useRef } from "react"
 
-const stringToColour = name => {
+const stringToColor = name => {
   let hash = 0
   for (let i = 0; i < name.length; i++) {
     hash = name.charCodeAt(i) + ((hash << 5) - hash)
@@ -106,8 +106,9 @@ const Chart = ({ data, margin }) => {
       svgElement
         .append("path")
         .datum(county.values)
+        .attr("class", "line")
         .attr("fill", "none")
-        .attr("stroke", stringToColour(county.name))
+        .attr("stroke", stringToColor(county.name))
         .attr("stroke-width", 1.5)
         .attr(
           "d",
@@ -121,13 +122,100 @@ const Chart = ({ data, margin }) => {
             })
         )
     })
+    var mouseG = svgElement.append("g").attr("class", "mouse-over-effects")
+
+    mouseG
+      .append("path") // this is the black vertical line to follow mouse
+      .attr("class", "mouse-line")
+      .style("stroke", "black")
+      .style("stroke-width", "1px")
+      .style("opacity", "0")
+
+    var lines = document.getElementsByClassName("line")
+
+    var mousePerLine = mouseG
+      .selectAll(".mouse-per-line")
+      .data(data)
+      .enter()
+      .append("g")
+      .attr("class", "mouse-per-line")
+
+    mousePerLine
+      .append("circle")
+      .attr("r", 7)
+      .style("stroke", function (d) {
+        return stringToColor(d.name)
+      })
+      .style("fill", "none")
+      .style("stroke-width", "1px")
+      .style("opacity", "0")
+
+    mousePerLine.append("text").attr("transform", "translate(10,3)")
+
+    mouseG
+      .append("svg:rect") // append a rect to catch mouse movements on canvas
+      .attr("width", svgWidth) // can't catch mouse events on a g element
+      .attr("height", height)
+      .attr("fill", "none")
+      .attr("pointer-events", "all")
+      .on("mouseout", function () {
+        // on mouse out hide line, circles and text
+        d3.select(".mouse-line").style("opacity", "0")
+        d3.selectAll(".mouse-per-line circle").style("opacity", "0")
+        d3.selectAll(".mouse-per-line text").style("opacity", "0")
+      })
+      .on("mouseover", function () {
+        // on mouse in show line, circles and text
+        d3.select(".mouse-line").style("opacity", "1")
+        d3.selectAll(".mouse-per-line circle").style("opacity", "1")
+        d3.selectAll(".mouse-per-line text").style("opacity", "1")
+      })
+      .on("mousemove", function () {
+        // mouse moving over canvas
+        var mouse = d3.mouse(this)
+        d3.select(".mouse-line").attr("d", function () {
+          var d = "M" + mouse[0] + "," + height
+          d += " " + mouse[0] + "," + 0
+          return d
+        })
+
+        d3.selectAll(".mouse-per-line").attr("transform", function (d, i) {
+          let xDate = x.invert(mouse[0]),
+            bisect = d3.bisector(function (d) {
+              return d.date
+            }).right
+          let idx = bisect(d.values, xDate)
+
+          let beginning = 0,
+            end = lines[i].getTotalLength(),
+            target = null
+          let pos = null
+          while (true) {
+            target = Math.floor((beginning + end) / 2)
+            pos = lines[i].getPointAtLength(target)
+            if (
+              (target === end || target === beginning) &&
+              pos.x !== mouse[0]
+            ) {
+              break
+            }
+            if (pos.x > mouse[0]) end = target
+            else if (pos.x < mouse[0]) beginning = target
+            else break //position found
+          }
+
+          d3.select(this).select("text").text(y.invert(pos.y).toFixed(2))
+
+          return "translate(" + mouse[0] + "," + pos.y + ")"
+        })
+      })
   }, [data, margin])
 
   return (
     <div>
       <div>
         {data.map(county => {
-          const color = stringToColour(county.name)
+          const color = stringToColor(county.name)
           return <Legend key={color} name={county.name} fill={color} />
         })}
       </div>
