7acf94ac1d648ec926a419566f8c55e5eaac7e9c
Damon Maneice <damaneice@gmail.com>
2020-07-31 00:08:38 -0400

    Add mobile menu icon to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,58 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div className="header">
-      <Link
-        className="title"
-        to="/"
-        style={{
-          color: `white`,
-          textDecoration: `none`,
-        }}
-      >
-        {siteTitle}
-      </Link>
-      <div className="header-right">
-        <a className="active" href="#home">
-          Home
-        </a>
-        <a href="#contact">Hospitalizations</a>
+const Header = ({ siteTitle }) => {
+  const [showMenu, setShowMenu] = useState(false)
+  return (
+    <header
+      style={{
+        marginBottom: `1.45rem`,
+      }}
+    >
+      <div className="header">
+        <button
+          className="mobile-menu-icon"
+          onClick={() => {
+            setShowMenu(!showMenu)
+          }}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+        </button>
+        <Link
+          className="title"
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+        <div className="header-right">
+          <Link activeClassName="active" to="/">
+            Home
+          </Link>
+          <Link activeClassName="active" to="/hospitalizations">
+            Hospitalizations{" "}
+          </Link>
+        </div>
+        {showMenu && (
+          <div>
+            <Link activeClassName="active" to="/">
+              Home
+            </Link>
+            <Link activeClassName="" to="/hospitalizations">
+              Hospitalizations{" "}
+            </Link>
+          </div>
+        )}
       </div>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
