bde2e5ae65008ed3630bbe67636a9eb3b84b8ab7
Damon Maneice <damaneice@gmail.com>
2020-07-30 23:33:05 -0400

    Add header

--- src/components/header.js
+++ src/components/header.js
@@ -5,34 +5,26 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
       marginBottom: `1.45rem`,
     }}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-        display: "flex",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent: "center",
-        alignItems: "center",
-        fontFamily: "avenir",
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+    <div className="header">
+      <Link
+        className="title"
+        to="/"
+        style={{
+          color: `white`,
+          textDecoration: `none`,
+        }}
+      >
+        {siteTitle}
+      </Link>
+      <div className="header-right">
+        <a className="active" href="#home">
+          Home
+        </a>
+        <a href="#contact">Hospitalizations</a>
+      </div>
     </div>
   </header>
 )
