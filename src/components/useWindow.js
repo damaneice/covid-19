d95a1099341ad0b15141ad747a9d55326be3cc5b
Damon Maneice <damaneice@github.com>
2020-08-04 14:53:56 -0400

    fix build issue with window not being defined during build

--- src/components/useWindow.js
+++ src/components/useWindow.js
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react"
 
 const useWindowSize = () => {
   const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: undefined,
+    height: undefined,
   })
 
   useEffect(() => {
