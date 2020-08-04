1d85a3f270e804d244620cae00d0f87f6042e90c
Damon Maneice <damaneice@github.com>
2020-08-04 14:42:54 -0400

    Connected real county case data to the map component

--- src/components/map.js
+++ src/components/map.js
@@ -1,8 +1,40 @@
 import * as d3 from "d3"
 import useWindow from "../components/useWindow"
 import React, { useEffect, useRef } from "react"
+import Legend from "./legend"
 
-const Map = ({ margin, name }) => {
+const colors = [
+  "rgb(235, 100, 52)",
+  "rgb(235, 158, 52)",
+  "rgb(235, 198, 52)",
+  "rgb(235, 232, 52)",
+  "rgb(201, 235, 52)",
+  "rgb(143, 235, 52)",
+  "rgb(120, 235, 52)",
+]
+
+const getColor = rateOfChage => {
+  let color = colors[3]
+  if (rateOfChage > 15) {
+    color = colors[0]
+  } else if (rateOfChage <= 15 && rateOfChage > 10) {
+    color = colors[1]
+  } else if (rateOfChage <= 10 && rateOfChage > 5) {
+    color = colors[2]
+  } else if (rateOfChage <= 5 && rateOfChage > 0) {
+    color = colors[3]
+  } else if (rateOfChage <= 5 && rateOfChage > 0) {
+    color = colors[3]
+  } else if (rateOfChage <= 0 && rateOfChage > -5) {
+    color = colors[4]
+  } else if (rateOfChage <= -5 && rateOfChage > -10) {
+    color = colors[5]
+  } else {
+    color = colors[6]
+  }
+  return color
+}
+const Map = ({ counties, margin, name }) => {
   const size = useWindow()
   const width = size.width + margin.left > 600 ? 600 : 300
   const height = size.width + margin.left > 600 ? 400 : 200
@@ -11,11 +43,6 @@ const Map = ({ margin, name }) => {
   useEffect(() => {
     async function fetchJSON() {
       const data = await import("../../static/michigan-counties.json")
-      const color = d3
-        .scaleThreshold()
-        .domain(d3.range(2, 10))
-        .range(d3.schemeBlues[9])
-
       const path = d3.geoPath().projection(
         d3
           .geoAlbers()
@@ -37,7 +64,7 @@ const Map = ({ margin, name }) => {
         .enter()
         .append("path")
         .attr("fill", function (d) {
-          return color(Math.floor(Math.random() * 10))
+          return getColor(counties[d.properties["NAME"]].rateOfChange)
         })
         .attr("d", path)
         .on("mouseenter", function (d) {
@@ -52,9 +79,20 @@ const Map = ({ margin, name }) => {
         })
     }
     fetchJSON()
-  }, [margin, name, width, height])
+  }, [counties, margin, name, width, height])
 
-  return <div style={{ textAlign: "center" }} ref={divRef}></div>
+  return (
+    <div style={{ textAlign: "center" }}>
+      <Legend name={"-15%"} fill={colors[6]} />
+      <Legend name={"-10%"} fill={colors[5]} />
+      <Legend name={"-5%"} fill={colors[4]} />
+      <Legend name={"0%"} fill={colors[3]} />
+      <Legend name={"5%"} fill={colors[2]} />
+      <Legend name={"10%"} fill={colors[1]} />
+      <Legend name={"15%"} fill={colors[0]} />
+      <div ref={divRef}></div>
+    </div>
+  )
 }
 
 export default Map
--- src/pages/highlights.js
+++ src/pages/highlights.js
@@ -5,7 +5,50 @@ import Map from "../components/map"
 import SEO from "../components/seo"
 import "./home.css"
 
-const HighlightsPage = () => {
+const countyCaseDataTransformer = data => {
+  const { edges } = data.allCasesByCountyAndDateCsvSheet1
+  const counties = {}
+  edges.forEach(edge => {
+    if (counties[edge.node.county]) {
+      counties[edge.node.county].newCases.push(parseInt(edge.node.newCases))
+    } else {
+      counties[edge.node.county] = { rateOfChange: 0, newCases: [] }
+    }
+  })
+  Object.keys(counties).forEach(county => {
+    //last 14 elements
+    const mostRecent = counties[county].newCases.slice(-14)
+    const previousRollingCases = []
+    const currentRollingCases = []
+    mostRecent.forEach((cases, index) => {
+      if (index < 7) {
+        previousRollingCases.push(cases)
+      } else {
+        currentRollingCases.push(cases)
+      }
+    })
+    let previousRollingCasesTotal = previousRollingCases.reduce(
+      (acc, current) => acc + current,
+      0
+    )
+    let currentRollingCasesTotal = currentRollingCases.reduce(
+      (acc, current) => acc + current,
+      0
+    )
+
+    const previousRollingAverage =
+      previousRollingCasesTotal / previousRollingCases.length || 1
+    const currentRollingAverage =
+      currentRollingCasesTotal / currentRollingCases.length || 1
+    counties[county].rateOfChange =
+      100 * (currentRollingAverage / previousRollingAverage - 1)
+  })
+
+  return counties
+}
+
+const HighlightsPage = ({ data }) => {
+  const counties = countyCaseDataTransformer(data)
   return (
     <Layout>
       <SEO title="Highlights" />
@@ -19,6 +62,7 @@ const HighlightsPage = () => {
         }}
       >
         <Map
+          counties={counties}
           name="Michigan"
           margin={{ top: 20, bottom: 80, right: 5, left: 40 }}
         />
@@ -26,5 +70,18 @@ const HighlightsPage = () => {
     </Layout>
   )
 }
+
+export const query = graphql`
+  query HighlightsQuery {
+    allCasesByCountyAndDateCsvSheet1 {
+      edges {
+        node {
+          county
+          newCases
+        }
+      }
+    }
+  }
+`
 
 export default HighlightsPage
