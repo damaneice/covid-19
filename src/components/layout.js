c7c6ee3a46f7aa1cd2fb18dcbe74edec0cedd1ed
Damon Maneice <damaneice@gmail.com>
2020-08-05 22:44:36 -0400

    Update data sources

--- src/components/layout.js
+++ src/components/layout.js
@@ -49,13 +49,25 @@ const Layout = ({ children }) => {
           justifyContent: "center",
           alignItems: "center",
           fontFamily: "avenir",
-          marginBottom: "20px",
+          marginBottom: "5px",
         }}
       >
-        <span>Data taken from the MDHHS Coronavirus website.</span>
-        <a href="https://www.michigan.gov/coronavirus">
-          www.michigan.gov/coronavirus
-        </a>
+        <span style={{ textAlign: "center" }}>
+          Data taken from the
+          <a
+            style={{ marginLeft: "4px", marginRight: "4px" }}
+            href="https://www.michigan.gov/coronavirus"
+          >
+            MDHHS Coronavirus website.
+          </a>
+          and the
+          <a
+            style={{ marginLeft: "4px" }}
+            href="https://github.com/nytimes/covid-19-data"
+          >
+            NYT GitHub repository.
+          </a>
+        </span>
       </footer>
     </>
   )
