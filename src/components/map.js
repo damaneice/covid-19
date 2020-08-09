b2225f7bf68527f2f5b4574ebd12f3b6ab1757f2
Damon Maneice <damaneice@gmail.com>
2020-08-08 22:43:34 -0400

    WIP

--- src/components/map.js
+++ src/components/map.js
@@ -11,21 +11,22 @@ const Map = ({ counties, getColor, margin, name }) => {
   useEffect(() => {
     async function fetchJSON() {
       const data = await import("../../static/michigan-counties.json")
-      const path = d3.geoPath().projection(
-        d3
-          .geoAlbers()
-          .translate([width / 2, height / 2])
-          .fitSize([width, height], data)
-      )
+      const projection = d3
+        .geoAlbers()
+        .translate([width / 2, height / 2])
+        .fitSize([width, height], data)
+      const path = d3.geoPath().projection(projection)
       const svgContainer = d3.select(divRef.current)
 
       //This is used to existing svg elements on resize
       svgContainer.selectAll("svg").remove()
 
-      svgContainer
+      const svg = svgContainer
         .append("svg")
         .attr("width", width + margin.left)
         .attr("height", height + margin.bottom)
+
+      svg
         .append("g")
         .selectAll("path")
         .data(data.features)
@@ -48,6 +49,47 @@ const Map = ({ counties, getColor, margin, name }) => {
             .style("stroke-width", 0.25)
             .style("stroke-dasharray", 1)
         })
+      const uniform = (min, max) => Math.random() * (max - min) + min
+      const generatePoint = feature => {
+        let point = []
+        let bounds = d3.geoBounds(feature)
+        while (point.length < 1) {
+          let minX = bounds[0][0]
+          let maxX = bounds[1][0]
+          let minY = bounds[0][1]
+          let maxY = bounds[1][1]
+          let tempPoint = [uniform(minX, maxX), uniform(minY, maxY)]
+
+          if (d3.geoContains(feature, tempPoint)) {
+            point = tempPoint
+          }
+        }
+        return point
+      }
+      const points = []
+      // setInterval(() => {
+      //   console.log("something")
+      //   data.features.forEach(feature => {
+      //     points.push(generatePoint(feature))
+      //   })
+
+      //   svg
+      //     .selectAll("myCircles")
+      //     .data(points)
+      //     .enter()
+      //     .append("circle")
+      //     .attr("cx", function (d) {
+      //       return projection([d[0], d[1]])[0]
+      //     })
+      //     .attr("cy", function (d) {
+      //       return projection([d[0], d[1]])[1]
+      //     })
+      //     .attr("r", 4)
+      //     .style("fill", "69b3a2")
+      //     .attr("stroke", "#69b3a2")
+      //     .attr("stroke-width", 3)
+      //     .attr("fill-opacity", 0.4)
+      // }, 500)
     }
     fetchJSON()
   }, [counties, getColor, margin, name, width, height])
