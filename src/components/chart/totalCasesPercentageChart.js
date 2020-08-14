6f097985103c440a54223eda266e266c6a960355
Damon Maneice <damaneice@gmail.com>
2020-08-14 00:36:45 -0400

    Add percentage positive chart

---
+++ src/components/chart/totalCasesPercentageChart.js
@@ -0,0 +1,54 @@
+import React from "react"
+import * as d3 from "d3"
+import ChartWithLegend from "./chartWithLegend"
+
+const groupStateCasesByDate = state => {
+  const groupeByDate = {}
+  state.chart.forEach(caseRecord => {
+    groupeByDate[caseRecord.date] = caseRecord
+  })
+  return groupeByDate
+}
+
+const createCasesChartData = (county, stateCasesGroupedByDate) => {
+  const chartData = []
+  for (let i = 0; i < county.chart.length; i++) {
+    const countyCasesDate = county.chart[i].date
+    const statesCases = stateCasesGroupedByDate[countyCasesDate].totalCases
+    chartData.push({
+      x: i,
+      y: (county.chart[i].totalCases / statesCases) * 100 || 0,
+      date: d3.timeParse("%Y-%m-%d")(countyCasesDate),
+    })
+  }
+  return chartData
+}
+
+const TotalCasesPercentageChart = ({
+  counties,
+  selectedNames,
+  updatedDate,
+}) => {
+  const stateCasesGroupedByDate = groupStateCasesByDate(counties["Michigan"])
+  const selectedCounties = selectedNames.map(name => {
+    return {
+      name: name,
+      values: createCasesChartData(counties[name], stateCasesGroupedByDate),
+    }
+  })
+
+  return (
+    <div>
+      {selectedCounties.length > 0 && (
+        <ChartWithLegend
+          name="Percentage of State's Total Cases"
+          margin={{ top: 20, bottom: 80, right: 5, left: 40 }}
+          data={selectedCounties}
+          updatedDate={updatedDate}
+        />
+      )}
+    </div>
+  )
+}
+
+export default TotalCasesPercentageChart
